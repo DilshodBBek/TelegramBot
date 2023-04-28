b021835c3ec5d25a15c8a7cba66d824875af8010
Dilshod <dilshodsh97@gmail.com>
2023-04-28 12:41:59 +0500

    updated

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -6,7 +6,7 @@ namespace TelegramBot1
 {
     internal class Program
     {
-        static TelegramBotClient bot = new("6268728289:AAGRIW0vItlqE15orLtlo2wKsnqBM20aBeI");
+        static TelegramBotClient bot = new("6208863954:AAEfO1MazHbA6aNXKo8Lh9vW66Vz-eX4ReQ");
         static void Main(string[] args)
         {
             bot.StartReceiving<UpdateHandler>();
@@ -17,60 +17,90 @@ namespace TelegramBot1
 
     class UpdateHandler : IUpdateHandler
     {
-        int a = 0;
-        long chatId = 0;
-        readonly User user = new();
+
+        List<Users> clients = new();
         public async Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
-            //  await botClient.SendTextMessageAsync(botClient.BotId, "Bu error:" + exception.Message);
+
         }
 
         public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            long _chatId = update.Message.Chat.Id;
-            await Console.Out.WriteLineAsync("start1");
-            Console.Out.WriteLineAsync(update.Message.Text+"ChatId:"+_chatId+"Name:"+update.Message.Chat.Username+ "Bio:" + update.Message.Chat.Bio);
-
-            if (chatId == 0 && a == 0 & update.Message.Text == "/AddUser")
+            Parallel.Invoke(() =>
             {
-                a = 3;
-                chatId = _chatId;
-                await botClient.SendTextMessageAsync(_chatId, "Ismni kiriting: "+ _chatId);
-            }
-            else if (chatId == _chatId && a == 3)
-            {
-                a--;
-                user.Name = update.Message.Text;
-                await botClient.SendTextMessageAsync(_chatId, "Emailni kiriting:");
-            }
-            else if (chatId == _chatId && a == 2)
-            {
-                a = 1;
-                user.Email = update.Message.Text;
-
-                await botClient.SendTextMessageAsync(_chatId, "Yoshni kiriting:");
-
-            }
-            else if (chatId == _chatId && a == 1)
-            {
-                user.Age = update.Message.Text;
-                a = 0;
-                chatId = 0;
-                await botClient.SendTextMessageAsync(_chatId, "Barcha ma`lumotlar kiritildi!\n"+user);
-
-            }
-           else await botClient.SendTextMessageAsync(_chatId, "Hi "+_chatId);
-
-            //await botClient.SendTextMessageAsync(update.Message.Chat.Username, "salom");
-
+                Task.Run(() => AddUser(botClient, update));
+            });
         }
-        record User
+        public async Task AddUser(ITelegramBotClient botClient, Update update)
         {
-            public string Name { get; set; }
-            public string Email { get; set; }
-            public string Age { get; set; }
+            try
+            {
+                if (clients.Exists(x => x.ChatId == update.Message.Chat.Id))
+                {
+                    await Handle(botClient, update);
+                }
+                else if (update.Message.Text.Equals("/AddUser", StringComparison.OrdinalIgnoreCase))
+                {
+                    clients.Add(new Users() { ChatId = update.Message.Chat.Id });
+                    await Handle(botClient, update);
+                }
+                else
+                {
+                    await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Hi ");
+                }
+            }
+            catch (Exception ex)
+            {
+                await Console.Out.WriteLineAsync(ex.Message);
+            }
+
+            async Task Handle(ITelegramBotClient botClient, Update update)
+            {
+                Users client = clients.First(x => x.ChatId == update.Message.Chat.Id);
+
+                long _chatId = update.Message.Chat.Id;
+
+                if (client.a == 0)
+                {
+                    client.a = 3;
+                    await botClient.SendTextMessageAsync(_chatId, "Ismni kiriting: ");
+                }
+                else if (client.a == 3)
+                {
+                    client.a--;
+                    client.user.Name = update.Message.Text;
+                    await botClient.SendTextMessageAsync(_chatId, "Emailni kiriting:");
+                }
+                else if (client.a == 2)
+                {
+                    client.a = 1;
+                    client.user.Email = update.Message.Text;
+
+                    await botClient.SendTextMessageAsync(_chatId, "Yoshni kiriting:");
+
+                }
+                else if (client.a == 1)
+                {
+                    client.user.Age = update.Message.Text;
+                    clients.Remove(client);
+                    await botClient.SendTextMessageAsync(_chatId, "Barcha ma`lumotlar kiritildi!\n" + client.user);
+
+                }
+            }
         }
 
 
+    }
+    record User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Age { get; set; }
+    }
+    class Users
+    {
+        public long ChatId { get; set; } = 0;
+        public byte a { get; set; } = 0;
+        public User user { get; set; } = new User();
     }
 }
